d975ff3cb307706f3195019ec452eac53c0b375d
Rafa Barranco Vela <rafabarrancovela@gmail.com>
2024-10-21 00:36:29 +0200

    Add css entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import AppRouter from './core/Router/index.tsx';
 
 import '@fontsource/inter';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
