d08fc374140965d42347e64285d08cd8b0b215d9
Rafa Barranco Vela <rafabarrancovela@gmail.com>
2024-10-21 00:34:46 +0200

    Add layout to router config

--- src/core/Router/index.tsx
+++ src/core/Router/index.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Employees from '../../Employees';
+import Layout from '../../containers/Layout';
 
 const AppRouter = () => {
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Employees />,
+      path: '',
+      element: <Layout />,
+      children: [{ path: '/', element: <Employees /> }],
     },
   ]);
   return <RouterProvider router={router} />;
