8ce03295c9dd799437578e7008cf7d2d12605efd
Rafa Barranco Vela <rafabarrancovela@gmail.com>
2024-10-22 07:24:05 +0200

    Refactor Employees container

--- src/views/Employees/constants.ts
+++
@@ -1,7 +0,0 @@
-import { TEmployeeRole } from '../../models/employees/types';
-
-export const ROLES_COLORS: Record<TEmployeeRole, string> = {
-  user: '#4c9baf95',
-  admin: '#f5a52387',
-  superadmin: '#f5454284',
-};
