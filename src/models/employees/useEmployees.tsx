e452fabb936d1acf538878ce541977369e824a37
Rafa Barranco Vela <rafabarrancovela@gmail.com>
2024-10-23 00:23:50 +0200

    Add create and edit functions to useEmployee custom hook

--- src/models/employees/useEmployees.tsx
+++ src/models/employees/useEmployees.tsx
@@ -3,7 +3,7 @@ import useApi from '../../core/api/useApi';
 import { IEmployee, IEmployeeDetails, IUseEmployeesResult } from './types';
 
 const useEmployees = (): IUseEmployeesResult => {
-  const { get } = useApi();
+  const { get, post, put } = useApi();
   const endpoint = 'employees';
 
   const getEmployees = async (): Promise<IEmployee[]> => {
@@ -22,7 +22,29 @@ const useEmployees = (): IUseEmployeesResult => {
     }
   };
 
-  return { getEmployees, getEmployeeDetails };
+  const addEmployee = async (body: IEmployeeDetails) => {
+    try {
+      return await post<IEmployeeDetails, IEmployeeDetails>(endpoint, body);
+    } catch (error) {
+      throw error as Error;
+    }
+  };
+
+  const editEmployee = async (
+    selectedEmployee: string,
+    body: IEmployeeDetails,
+  ) => {
+    try {
+      return await put<IEmployeeDetails, IEmployeeDetails>(
+        `${endpoint}/${selectedEmployee}`,
+        body,
+      );
+    } catch (error) {
+      throw error as Error;
+    }
+  };
+
+  return { getEmployees, getEmployeeDetails, addEmployee, editEmployee };
 };
 
 export default useEmployees;
