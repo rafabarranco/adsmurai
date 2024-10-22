3733537c915fa2778aea528c17cd8bc51cd6a2f6
Rafa Barranco Vela <rafabarrancovela@gmail.com>
2024-10-22 05:24:35 +0200

    Refactor and testing employee details view

--- src/views/Employees/Details/index.tsx
+++ src/views/Employees/Details/index.tsx
@@ -1,9 +1,24 @@
 import { FC, ReactElement, useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import {
+  AspectRatio,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  Sheet,
+  Grid,
+} from '@mui/joy';
+
 import useEmployees from '../../../models/employees/useEmployees';
 
+import { DEPARTMENTS } from './constants';
+import { ROLES } from '../constants';
+
 import { Employee } from '../../../models/employees/types';
+import { Departments } from './types';
+import styles from './styles';
 
 const EmployeeDetails: FC = (): ReactElement => {
   const { id } = useParams();
@@ -26,117 +41,74 @@ const EmployeeDetails: FC = (): ReactElement => {
   }, []);
 
   return (
-    <div>Employee details: {employeeDetails && employeeDetails.email}</div>
+    <Card orientation="horizontal" sx={styles.Card}>
+      <AspectRatio flex ratio="1" sx={styles.Picture}>
+        <img
+          src={employeeDetails.picture}
+          loading="lazy"
+          alt={`${employeeDetails.firstName} ${employeeDetails.lastName}`}
+        />
+      </AspectRatio>
+      <CardContent sx={styles.CardContent}>
+        <Grid sx={styles.Info}>
+          <Typography sx={styles.Name}>
+            {employeeDetails.firstName} {employeeDetails.lastName}
+          </Typography>
+          <Typography sx={styles.Id}>{employeeDetails.id}</Typography>
+        </Grid>
+
+        <Typography level="body-sm" textColor="text.tertiary" sx={styles.Email}>
+          {employeeDetails.email}
+        </Typography>
+        <Sheet sx={styles.Sheet}>
+          <Grid>
+            <Typography level="body-xs" sx={styles.DepartmentLabel}>
+              Department
+            </Typography>
+            <Typography sx={styles.DepartmentValue}>
+              {employeeDetails.department &&
+                DEPARTMENTS[employeeDetails.department as Departments]}{' '}
+              {employeeDetails.department?.toLocaleUpperCase()}
+            </Typography>
+          </Grid>
+          <Grid>
+            <Typography level="body-xs" sx={styles.RoleLabel}>
+              Role
+            </Typography>
+            <Typography
+              sx={{ ...styles.RoleValue, color: ROLES[employeeDetails.role] }}
+            >
+              {employeeDetails.role?.toLocaleUpperCase()}
+            </Typography>
+          </Grid>
+          <Grid>
+            <Typography level="body-xs" sx={styles.SalaryLabel}>
+              Salary
+            </Typography>
+            <Typography sx={styles.SalaryValue}>
+              {`${employeeDetails.salary}€`}
+            </Typography>
+          </Grid>
+        </Sheet>
+        <Grid sx={styles.HireInfo}>
+          <Button variant="outlined" color="neutral" sx={styles.HireDate}>
+            {employeeDetails.hireDate
+              ? employeeDetails.hireDate.substring(0, 10)
+              : 'Unknown'}
+          </Button>
+          <Button
+            variant="solid"
+            color={employeeDetails.dismissalDate ? 'danger' : 'success'}
+            sx={styles.DismissalDate}
+          >
+            {employeeDetails.dismissalDate
+              ? employeeDetails.dismissalDate.substring(0, 10)
+              : 'Currently hired'}
+          </Button>
+        </Grid>
+      </CardContent>
+    </Card>
   );
-  // const { employee } = useContext(DataContext) as any;
-  // const {
-  //   id,
-  //   picture,
-  //   firstName,
-  //   lastName,
-  //   email,
-  //   role,
-  //   department,
-  //   salary,
-  //   hireDate,
-  //   dismissalDate,
-  // } = (employee as any) || {};
-  // const dateOfHire = hireDate ? hireDate.substring(0, 10) : 'Unknown';
-  // const dateOfDismissal = dismissalDate
-  //   ? dismissalDate.substring(0, 10)
-  //   : 'Currently hired';
-  // const Tag = () => {
-  //   if (role == 'user') {
-  //     return <div className="tag user">User</div>;
-  //   } else if (role == 'admin') {
-  //     return <div className="tag admin">Admin</div>;
-  //   } else if (role == 'superadmin') {
-  //     return <div className="tag superadmin">Superadmin</div>;
-  //   }
-  // };
-  // const DepartamentWithIcon = (): ReactNode => {
-  //   console.log(department);
-  //   if (department == 'customer success') {
-  //     return (
-  //       <div className="departament">
-  //         <div className="material-icons">support_agent</div>
-  //         Customer Success
-  //       </div>
-  //     );
-  //   } else if (department == 'engineering') {
-  //     return (
-  //       <div className="departament">
-  //         <div className="material-icons">computer</div>
-  //         IT
-  //       </div>
-  //     );
-  //   } else if (department == 'finance') {
-  //     return (
-  //       <div className="departament">
-  //         <div className="material-icons">account_balance</div>
-  //         Finance
-  //       </div>
-  //     );
-  //   }
-  //   return <>{department}</>;
-  // };
-  // return (
-  //   <div>
-  //     <div
-  //       className="button"
-  //       onClick={() => {
-  //         handleGoToEmployeesPage();
-  //       }}
-  //     >
-  //       Click here to go back
-  //     </div>
-  //     <div className="employee">
-  //       <div>{id}</div>
-  //       <div className="imageWrapper">
-  //         <img src={picture} width={'250px'} height={'250px'} />
-  //       </div>
-  //       <div className="employeeDetailGroup">
-  //         <div>
-  //           <div>Name:</div>
-  //           <div>{firstName}</div>
-  //         </div>
-  //         <div>
-  //           <div>Last name:</div>
-  //           <div>{lastName}</div>
-  //         </div>
-  //         <div>
-  //           <div>Email:</div>
-  //           <div>{email}</div>
-  //         </div>
-  //       </div>
-  //       <div className="employeeDetailGroup">
-  //         <div>
-  //           <div>Role:</div>
-  //           <div>{Tag()}</div>
-  //         </div>
-  //         <div>
-  //           <div>Department:</div>
-  //           <div>{DepartamentWithIcon()}</div>
-  //         </div>
-  //         <div>
-  //           <div>Salary:</div>
-  //           <div>{salary} €</div>
-  //         </div>
-  //       </div>
-  //       <div className="employeeDetailGroup">
-  //         <div>
-  //           <div>Hired on:</div>
-  //           <div>{dateOfHire}</div>
-  //         </div>
-  //         <div>
-  //           <div>Dismissal date:</div>
-  //           <div>{dateOfDismissal}</div>
-  //         </div>
-  //       </div>
-  //       {/* <div className='employeeDetailGroup'></div> */}
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default EmployeeDetails;
---
+++ src/views/Employees/Details/types.ts
@@ -0,0 +1,19 @@
+export type Departments = 'customer success' | 'engineering' | 'finance';
+export type TStyles =
+  | 'Card'
+  | 'CardContent'
+  | 'Picture'
+  | 'Info'
+  | 'Name'
+  | 'Id'
+  | 'Email'
+  | 'Sheet'
+  | 'DepartmentLabel'
+  | 'DepartmentValue'
+  | 'RoleLabel'
+  | 'RoleValue'
+  | 'SalaryLabel'
+  | 'SalaryValue'
+  | 'HireInfo'
+  | 'HireDate'
+  | 'DismissalDate';
