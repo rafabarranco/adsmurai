af802aac0537d43e131f3c3931f52facc83fe814
Rafa Barranco Vela <rafabarrancovela@gmail.com>
2024-10-23 07:29:19 +0200

    Delete usePrompt hook

--- src/utils/hooks/usePrompt/types.ts
+++
@@ -1,9 +0,0 @@
-import { Transition } from 'history';
-
-interface HistoryNavigator {
-  block: (callback: (tx: Transition) => void) => () => void;
-}
-
-export interface NavigationContextObject {
-  navigator: HistoryNavigator;
-}
